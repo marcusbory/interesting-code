614a2d96edcdcca8a0cc14f1f5a4a6d4335b0a83
Marcus Ong <ong1.marcusb@gmail.com>
2021-04-10 00:46:55 +0800

    implemented delete for linear probe

--- Hash/LinearProbe.cpp
+++ Hash/LinearProbe.cpp
@@ -11,6 +11,7 @@ private:
 public:
     HashTable(int cap);
     void insert(int key);
+    void deleteKey(int key);
     int search(int key);
     int hash(int key);
     int linearProbe(int key);
@@ -53,6 +54,16 @@ int main() {
                     cout << keySearch << " is found at index " << ht->search(keySearch) << endl;
                 break;
             case 4:
+                cout << "Enter key to delete: ";
+                int keyDel; cin >> keyDel;
+                if (ht->search(keyDel) == -1)
+                    cout << "Key not found :(" << endl;
+                else {
+                    ht->deleteKey(keyDel);
+                    cout << "Key " << keyDel << " successfully deleted." << endl;
+                }
+                break;
+            case 5:
                 cout << "Goodbye!" << endl;
                 delete ht;
                 exit(0);
@@ -84,10 +95,16 @@ int HashTable::linearProbe(int key) {
 void HashTable::insert(int key) {
     int base, insertIndex;
     base = insertIndex = hash(key);
-    if (_ht[base] != EMPTY)
+    if (_ht[base] != EMPTY && _ht[base] != DEL)
         insertIndex = linearProbe(key);
     _ht[insertIndex] = key;
     _load++;
+}
+
+void HashTable::deleteKey(int key) {
+    int deleteIndex = search(key);
+    if (deleteIndex == -1) return;
+    _ht[deleteIndex] = DEL;    
 }
 
 int HashTable::search(int key) {
@@ -105,6 +122,8 @@ void HashTable::printTable() {
     for (int i = 0; i < _capacity; i++) {
         if (_ht[i] == EMPTY)
             cout << "EMPTY ";
+        else if (_ht[i] == DEL)
+            cout << "DEL ";
         else
             cout << _ht[i] << " ";
     }
@@ -115,6 +134,7 @@ void menu() {
     cout << "Option 1. Check hash table." << endl;
     cout << "Option 2. Insert a key. (Only if hash table is not full!)" << endl;
     cout << "Option 3. Search for the index of a key." << endl;
-    cout << "Option 4. Quit." << endl;
+    cout << "Option 4. Delete a key (existing)." << endl;
+    cout << "Option 5. Quit." << endl;
     cout << "What do you want to do? ";
 }
